5351aebacc0d474c9ad3818721be0f9b4165f8d6
Karen Beltran-Lopez <beltlok@gmail.com>
2018-09-15 20:44:39 -0500

    added hw files

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const exphbs = require('express-handlebars'),
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+
+app.use(express.static("public"));
+
+const config = require("./config/database");
+mongoose.Promise = Promise;
+mongoose.connect(config.database)
+  .then(result => {
+    console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+  })
+  .catch(err => console.log('There was an error with your connection:', err));
+
+require('./controllers/index')(app),
+require('./controllers/notes')(app),
+require('./controllers/articles')(app),
+require('./controllers/scrape')(app);
+
+
+//start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
